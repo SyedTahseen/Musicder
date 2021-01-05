daa1be4e95503976561ebe23b65bbe8ad89000d0
Syed Tahseen <53108646+SyedTahseen@users.noreply.github.com>
2021-01-05 13:30:08 +0500

    Update PlayScreen.js

--- src/screens/PlayScreen.js
+++ src/screens/PlayScreen.js
@@ -36,9 +36,9 @@ export default class PlayScreen extends React.Component {
                 var song = res.data;
                 this.setState({ song });
                 if (song.result === "false") {
-                    document.title = `Error | Musicder`
+                    document.title = `Error | Audiofy`
                 } else {
-                    document.title = `${song.song} by ${song.singers} | Musicder`
+                    document.title = `${song.song} by ${song.singers} | Audiofy`
                 }
             })
     }
@@ -63,7 +63,7 @@ export default class PlayScreen extends React.Component {
                     <div className="playhead">
                         <h1 className="playheadplay">Play</h1>
                         <Link to="../">
-                            <img alt="Musicder" src="../img/t-logo.svg" className="playlogobtn" width="60" height="60" />
+                            <img alt="Audiofy" src="../img/t-logo.svg" className="playlogobtn" width="60" height="60" />
                         </Link>
                     </div>
                     <div className="playcontent"><br /><br />
@@ -82,11 +82,11 @@ export default class PlayScreen extends React.Component {
                         {this.state.lyrics}
                     </div>
                     <div className="footer">
-                        <h1 className="playlogo">MUSICDER</h1> <br />
-                        <a className="atextdec" href={`https://github.com/cachecleanerjeet`}>
-                            <p className="playparagone" >An Open Source Project by Tuhin</p>
+                        <h1 className="playlogo">Audiofy</h1> <br />
+                        <a className="atextdec" href={`https://facebook.com/iTahseen`}>
+                            <p className="playparagone" >Made in ❤</p>
                         </a><br />
-                        <a href={`https://github.com/cachecleanerjeet/Musicder`}>
+                        <a href={`https://github.com/SyedTahseen`}>
                             <img alt="Github" className="center" src="../img/github-black.svg" width="30" height="30" /><br />
                         </a>
                     </div>
